e0d3feacf240854f55a48eefaf8a36d3a969a590
Tyler Hancock <darklime@live.ca>
2023-07-22 20:50:46 -0600

    Revert syncing on tag updates.

--- src/main/java/net/darkhax/itemstages/jei/PluginItemStages.java
+++ src/main/java/net/darkhax/itemstages/jei/PluginItemStages.java
@@ -1,10 +1,6 @@
 package net.darkhax.itemstages.jei;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import com.ibm.icu.text.DecimalFormat;
-
 import mezz.jei.api.IModPlugin;
 import mezz.jei.api.JeiPlugin;
 import mezz.jei.api.constants.VanillaTypes;
@@ -17,54 +13,54 @@ import net.darkhax.itemstages.ItemStages;
 import net.darkhax.itemstages.Restriction;
 import net.darkhax.itemstages.RestrictionManager;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.client.event.RecipesUpdatedEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.event.TagsUpdatedEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.fml.util.thread.EffectiveSide;
-import org.jetbrains.annotations.NotNull;
 
-@SuppressWarnings("unused")
+import java.util.ArrayList;
+import java.util.List;
+
 @JeiPlugin
 @OnlyIn(Dist.CLIENT)
 public class PluginItemStages implements IModPlugin {
-    
+
     private static final ResourceLocation PLUGIN_ID = new ResourceLocation("itemstages", "main");
     private static final DecimalFormat FORMAT = new DecimalFormat("#.##");
-    
+
     private IJeiRuntime runtime;
     private final List<ItemStack> hiddenItems = new ArrayList<>();
-    
+
     public PluginItemStages() {
-        
+
         if (EffectiveSide.get().isClient()) {
-            
+
             MinecraftForge.EVENT_BUS.addListener(EventPriority.NORMAL, false, StagesSyncedEvent.class, e -> this.updateHiddenItems());
             MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, RecipesUpdatedEvent.class, e -> this.updateHiddenItems());
-            MinecraftForge.EVENT_BUS.addListener(EventPriority.LOWEST, false, TagsUpdatedEvent.class, e -> this.updateHiddenItems());
         }
     }
-    
+
     @Override
-    public ResourceLocation getPluginUid () {
-        
+    public ResourceLocation getPluginUid() {
+
         return PLUGIN_ID;
     }
-    
+
     @Override
-    public void onRuntimeAvailable (IJeiRuntime jeiRuntime) {
-        
+    public void onRuntimeAvailable(IJeiRuntime jeiRuntime) {
+
         this.runtime = jeiRuntime;
     }
-    
-    private void updateHiddenItems () {
-        
+
+    private void updateHiddenItems() {
+
         if (this.runtime != null) {
+
             this.runtime.getIngredientManager();
 
             final long syncStart = System.nanoTime();
@@ -79,55 +75,55 @@ public class PluginItemStages implements IModPlugin {
             ItemStages.LOGGER.debug("JEI has been synced with ItemStages. Took {}ms.", FORMAT.format((System.nanoTime() - syncStart) / 1000000));
         }
     }
-    
-    private void restoreStagedItems (IIngredientManager ingredients) {
-        
+
+    private void restoreStagedItems(IIngredientManager ingredients) {
+
         // Restore the JEI ingredient list to it's previous state.
         final long restoreStart = System.nanoTime();
         ItemStages.LOGGER.debug("Restoring {} hidden items.", this.hiddenItems.size());
-        
+
         if (!this.hiddenItems.isEmpty()) {
-            
+
             ingredients.addIngredientsAtRuntime(VanillaTypes.ITEM_STACK, this.hiddenItems);
             this.hiddenItems.clear();
         }
-        
+
         ItemStages.LOGGER.debug("Items list restored. Took {}ms.", FORMAT.format((System.nanoTime() - restoreStart) / 1000000));
     }
-    
-    private void collectStagedIngredients (IIngredientManager ingredients) {
-        
+
+    private void collectStagedIngredients(IIngredientManager ingredients) {
+
         // Calculate the list of items to hide from JEI.
         ItemStages.LOGGER.debug("Calculating items to hide.");
         final long hideCalcStart = System.nanoTime();
         final RestrictionManager restrictions = RestrictionManager.INSTANCE;
         final Player player = Minecraft.getInstance().player;//TODO PlayerUtils.getClientPlayer();
         final IStageData stageData = GameStageHelper.getPlayerData(player);
-        
+
         for (final ItemStack ingredient : ingredients.getAllIngredients(VanillaTypes.ITEM_STACK)) {
-            
+
             final Restriction restriction = restrictions.getRestriction(player, stageData, ingredient);
-            
+
             if (restriction != null && restriction.shouldHideInJEI()) {
-                
+
                 this.hiddenItems.add(ingredient);
             }
         }
-        
+
         ItemStages.LOGGER.debug("Marked {} entries for hiding. Took {}ms.", this.hiddenItems.size(), FORMAT.format((System.nanoTime() - hideCalcStart) / 1000000));
     }
-    
-    private void hideStagedIngredients (IIngredientManager ingredients) {
-        
+
+    private void hideStagedIngredients(IIngredientManager ingredients) {
+
         // Hide hidden entries from JEI.
         ItemStages.LOGGER.debug("Hiding {} entries from JEI.", this.hiddenItems.size());
         final long hideStart = System.nanoTime();
-        
+
         if (!this.hiddenItems.isEmpty()) {
-            
+
             ingredients.removeIngredientsAtRuntime(VanillaTypes.ITEM_STACK, this.hiddenItems);
         }
-        
+
         ItemStages.LOGGER.debug("All entries hidden. Took {}ms.", FORMAT.format((System.nanoTime() - hideStart) / 1000000));
     }
 }
