2ceaff5b6cdd7b23c56be86f8116db7d07ca967c
Tyler Hancock <darklime@live.ca>
2024-03-26 17:43:39 -0600

    Fix restrictions created with CraftTweaker scripts not being created on dedicated clients.

--- src/main/java/net/darkhax/itemstages/crt/actions/ActionCreateRestriction.java
+++ src/main/java/net/darkhax/itemstages/crt/actions/ActionCreateRestriction.java
@@ -2,27 +2,29 @@ package net.darkhax.itemstages.crt.actions;
 
 import com.blamejared.crafttweaker.api.CraftTweakerAPI;
 import com.blamejared.crafttweaker.api.action.base.IRuntimeAction;
+import com.blamejared.crafttweaker.api.zencode.IScriptLoadSource;
 import net.darkhax.itemstages.Restriction;
 import net.darkhax.itemstages.RestrictionManager;
+import org.apache.logging.log4j.Logger;
 
 public final class ActionCreateRestriction implements IRuntimeAction {
-    
+
     private final Restriction toRegister;
-    
+
     private ActionCreateRestriction(Restriction toRegister) {
-        
+
         this.toRegister = toRegister;
     }
-    
+
     @Override
-    public void apply () {
-        
+    public void apply() {
+
         RestrictionManager.INSTANCE.addRestriction(this.toRegister);
     }
-    
+
     @Override
-    public String describe () {
-        
+    public String describe() {
+
         return "[ItemStages] Creating new restriction for stages " + this.toRegister.getStages() + ".";
     }
 
@@ -31,8 +33,14 @@ public final class ActionCreateRestriction implements IRuntimeAction {
         return "Item Stages Restrictions";
     }
 
-    public static void apply (Restriction toRegister) {
-        
+    @Override
+    public boolean shouldApplyOn(final IScriptLoadSource source, final Logger logger) {
+
+        return true;
+    }
+
+    public static void apply(Restriction toRegister) {
+
         CraftTweakerAPI.apply(new ActionCreateRestriction(toRegister));
     }
 }
